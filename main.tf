e906e0f23ec0fb1903916c17e120c0e58becc60c
heejin <dlwlrgkwk82@gmail.com>
2025-02-14 01:59:55 +0900

    feat: add ec2 creation

--- main.tf
+++ main.tf
@@ -10,3 +10,12 @@ terraform {
 provider "aws" {
   region = "ap-northeast-2"
 }
+
+resource "aws_instance" "tiny-url-instance" {
+  ami           = "ami-024ea438ab0376a47"
+  instance_type = "t2.micro"
+  
+  tags = {
+    Name = "tiny-url-instance"
+  }
+}
